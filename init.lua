2223dd656c240df91c4bb4e741d2652263723cd4
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-05-19 20:49:21 +0700

    add config

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("vim._core.ui2").enable({})
+
+require("options")
+require("keymaps")
+require("commands")
+require("pack")
+require("treesitter")
+require("lsp")
+
+vim.cmd.colorscheme("moonfly")
